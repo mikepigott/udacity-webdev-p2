b0aa346a90c3a1d045675a3c642527c33e2c78e2
mikepigott <mpigott@ironponyexpress.org>
2015-05-06 08:01:28 -0400

    Correcting indentation and adding semicolons per code review.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -16,32 +16,32 @@ var bio = {
   ],
   "bioPic": "images/profile.jpg",
   "display": function() {
-      $("#header").prepend(HTMLheaderRole.replace(dataTmpl, bio.role));
-      $("#header").prepend(HTMLheaderName.replace(dataTmpl, bio.name));
+    $("#header").prepend(HTMLheaderRole.replace(dataTmpl, bio.role));
+    $("#header").prepend(HTMLheaderName.replace(dataTmpl, bio.name));
 
-      $("#topContacts").append(HTMLmobile.replace(dataTmpl, bio.contacts.mobile));
-      $("#topContacts").append(HTMLemail.replace(dataTmpl, bio.contacts.email));
-      $("#topContacts").append(HTMLtwitter.replace(dataTmpl, bio.contacts.twitter));
-      $("#topContacts").append(HTMLgithub.replace(dataTmpl, bio.contacts.github));
-      $("#topContacts").append(HTMLlocation.replace(dataTmpl, bio.contacts.location));
+    $("#topContacts").append(HTMLmobile.replace(dataTmpl, bio.contacts.mobile));
+    $("#topContacts").append(HTMLemail.replace(dataTmpl, bio.contacts.email));
+    $("#topContacts").append(HTMLtwitter.replace(dataTmpl, bio.contacts.twitter));
+    $("#topContacts").append(HTMLgithub.replace(dataTmpl, bio.contacts.github));
+    $("#topContacts").append(HTMLlocation.replace(dataTmpl, bio.contacts.location));
 
-      $("#header").append(HTMLwelcomeMsg.replace(dataTmpl, bio.welcomeMessage));
-      $("#header").append(HTMLbioPic.replace(dataTmpl, bio.bioPic));
+    $("#header").append(HTMLwelcomeMsg.replace(dataTmpl, bio.welcomeMessage));
+    $("#header").append(HTMLbioPic.replace(dataTmpl, bio.bioPic));
 
-      if (bio.skills && bio.skills.length > 0) {
-          $("#header").append(HTMLskillsStart);
-          for (skill in bio.skills) {
-              $("#skills").append(HTMLskills.replace(dataTmpl, bio.skills[skill]));
-          }
-      }
+    if (bio.skills && bio.skills.length > 0) {
+      $("#header").append(HTMLskillsStart);
+        for (skill in bio.skills) {
+          $("#skills").append(HTMLskills.replace(dataTmpl, bio.skills[skill]));
+        }
+    }
 
-      $("#footerContacts").append(HTMLmobile.replace(dataTmpl, bio.contacts.mobile));
-      $("#footerContacts").append(HTMLemail.replace(dataTmpl, bio.contacts.email));
-      $("#footerContacts").append(HTMLtwitter.replace(dataTmpl, bio.contacts.twitter));
-      $("#footerContacts").append(HTMLgithub.replace(dataTmpl, bio.contacts.github));
-      $("#footerContacts").append(HTMLlocation.replace(dataTmpl, bio.contacts.location));
+    $("#footerContacts").append(HTMLmobile.replace(dataTmpl, bio.contacts.mobile));
+    $("#footerContacts").append(HTMLemail.replace(dataTmpl, bio.contacts.email));
+    $("#footerContacts").append(HTMLtwitter.replace(dataTmpl, bio.contacts.twitter));
+    $("#footerContacts").append(HTMLgithub.replace(dataTmpl, bio.contacts.github));
+    $("#footerContacts").append(HTMLlocation.replace(dataTmpl, bio.contacts.location));
   }
-}
+};
 
 var education = {
   "schools": [
@@ -63,37 +63,37 @@ var education = {
     }
   ],
   "display": function() {
-      if (education.schools && education.schools.length > 0) {
-          for (school in education.schools) {
-          $("#education").append(HTMLschoolStart);
+    if (education.schools && education.schools.length > 0) {
+      for (school in education.schools) {
+        $("#education").append(HTMLschoolStart);
 
-          var formattedName =
-                HTMLschoolName.replace(dataTmpl, education.schools[school].name)
-                  + HTMLschoolDegree.replace(dataTmpl, education.schools[school].degree);
+        var formattedName =
+          HTMLschoolName.replace(dataTmpl, education.schools[school].name)
+            + HTMLschoolDegree.replace(dataTmpl, education.schools[school].degree);
 
-            $(".education-entry:last").append(formattedName);
-            $(".education-entry:last").append(HTMLschoolDates.replace(dataTmpl, education.schools[school].dates));
-            $(".education-entry:last").append(HTMLschoolLocation.replace(dataTmpl, education.schools[school].location));
-            $(".education-entry:last").append(HTMLschoolMajor.replace(dataTmpl, education.schools[school].major));
-        }
+        $(".education-entry:last").append(formattedName);
+        $(".education-entry:last").append(HTMLschoolDates.replace(dataTmpl, education.schools[school].dates));
+        $(".education-entry:last").append(HTMLschoolLocation.replace(dataTmpl, education.schools[school].location));
+        $(".education-entry:last").append(HTMLschoolMajor.replace(dataTmpl, education.schools[school].major));
+      }
     }
 
     if (education.onlineCourses && education.onlineCourses.length > 0) {
-        $("#education").append(HTMLonlineClasses);
-        for (course in education.onlineCourses) {
-            $("#education").append(HTMLschoolStart);
+      $("#education").append(HTMLonlineClasses);
+      for (course in education.onlineCourses) {
+        $("#education").append(HTMLschoolStart);
 
-            var formattedName =
-                HTMLonlineTitle.replace(dataTmpl, education.onlineCourses[course].title)
-                + HTMLonlineSchool.replace(dataTmpl, education.onlineCourses[course].school);
+        var formattedName =
+          HTMLonlineTitle.replace(dataTmpl, education.onlineCourses[course].title)
+            + HTMLonlineSchool.replace(dataTmpl, education.onlineCourses[course].school);
 
-            $(".education-entry:last").append(formattedName);
-            $(".education-entry:last").append(HTMLonlineDates.replace(dataTmpl, education.onlineCourses[course].dates));
-            $(".education-entry:last").append(HTMLonlineURL.replace(dataTmpl, education.onlineCourses[course].url));
-        }
+        $(".education-entry:last").append(formattedName);
+        $(".education-entry:last").append(HTMLonlineDates.replace(dataTmpl, education.onlineCourses[course].dates));
+        $(".education-entry:last").append(HTMLonlineURL.replace(dataTmpl, education.onlineCourses[course].url));
+      }
     }
   }
-}
+};
 
 var work = {
   "jobs": [
@@ -115,11 +115,11 @@ var work = {
       "location": "350 5th Ave, New York, NY 10118"
     },
     {
-       "employer": "FactSet Research Systems, Inc.",
-       "title": "Senior Software Engineer",
-       "dates": "July 2005 - February 2013",
-       "description": "Responsible for designing and prototyping a new data storage format in HBase for performance comparison of financial time-series data against the FactSet custom financial time-series database. Developed a data transfer mechanism from existing FactSet custom databases to HBase using C++, Perl, Java, ANTLR, and Map/Reduce.  Managed a team of six engineers in the Quantitative Applications group to enhance and maintain FactSet's Alpha Testing, Portfolio Simulation, and Screen Iterator products, all written in C++. Worked with three Product Developers over two offices to prioritize both major projects and minor enhancements for those applications. Also initiated a side project to prototype a performance improvement of Alpha Testing via Map/Reduce on Hbase using C++, Thirft, Perl, Java, and Hadoop Map/Reduce.  Designed, developed, launched, and enhanced PA Reconciliation, a C++ application designed to find and report discrepancies in client data. Also maintained and enhanced a C++ parser that collects financial trades directly from the Advent Axys / APX accounting system.",
-       "location": "601 Merritt Seven, Norwalk, Connecticut, 06851",
+      "employer": "FactSet Research Systems, Inc.",
+      "title": "Senior Software Engineer",
+      "dates": "July 2005 - February 2013",
+      "description": "Responsible for designing and prototyping a new data storage format in HBase for performance comparison of financial time-series data against the FactSet custom financial time-series database. Developed a data transfer mechanism from existing FactSet custom databases to HBase using C++, Perl, Java, ANTLR, and Map/Reduce.  Managed a team of six engineers in the Quantitative Applications group to enhance and maintain FactSet's Alpha Testing, Portfolio Simulation, and Screen Iterator products, all written in C++. Worked with three Product Developers over two offices to prioritize both major projects and minor enhancements for those applications. Also initiated a side project to prototype a performance improvement of Alpha Testing via Map/Reduce on Hbase using C++, Thirft, Perl, Java, and Hadoop Map/Reduce.  Designed, developed, launched, and enhanced PA Reconciliation, a C++ application designed to find and report discrepancies in client data. Also maintained and enhanced a C++ parser that collects financial trades directly from the Advent Axys / APX accounting system.",
+      "location": "601 Merritt Seven, Norwalk, Connecticut, 06851",
       "accomplishments": [
         "Reduced a 1,100ms fetch time from a custom time-series database to 475ms on HBase with the new storage format.",
         "Recommended further system-wide changes to remove 300ms of fetch time from the remaining 475ms.",
@@ -131,64 +131,64 @@ var work = {
     }
   ],
   "display": function displayWork() {
-      if (work.jobs && work.jobs.length > 0) {
-          for (job in work.jobs) {
-              $("#workExperience").append(HTMLworkStart);
+    if (work.jobs && work.jobs.length > 0) {
+      for (job in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
 
-              var formattedName =
-                  HTMLworkEmployer.replace(dataTmpl, work.jobs[job].employer)
-                  + HTMLworkTitle.replace(dataTmpl, work.jobs[job].title);
+        var formattedName =
+          HTMLworkEmployer.replace(dataTmpl, work.jobs[job].employer)
+            + HTMLworkTitle.replace(dataTmpl, work.jobs[job].title);
 
-              $(".work-entry:last").append(formattedName);
-              $(".work-entry:last").append(HTMLworkDates.replace(dataTmpl, work.jobs[job].dates));
-              $(".work-entry:last").append(HTMLworkLocation.replace(dataTmpl, work.jobs[job].location));
-              $(".work-entry:last").append(HTMLworkDescription.replace(dataTmpl, work.jobs[job].description));
-              if (work.jobs[job].accomplishments && work.jobs[job].accomplishments.length > 0) {
-                  $(".work-entry:last").append(HTMLworkAccomplishmentsStart);
-                  for (accomplishment in work.jobs[job].accomplishments) {
-                      $(".accomplishments:last").append(HTMLworkAccomplishmentsEntry.replace(dataTmpl, work.jobs[job].accomplishments[accomplishment]));
-                  }
-              }
+        $(".work-entry:last").append(formattedName);
+        $(".work-entry:last").append(HTMLworkDates.replace(dataTmpl, work.jobs[job].dates));
+        $(".work-entry:last").append(HTMLworkLocation.replace(dataTmpl, work.jobs[job].location));
+        $(".work-entry:last").append(HTMLworkDescription.replace(dataTmpl, work.jobs[job].description));
+        if (work.jobs[job].accomplishments && work.jobs[job].accomplishments.length > 0) {
+          $(".work-entry:last").append(HTMLworkAccomplishmentsStart);
+          for (accomplishment in work.jobs[job].accomplishments) {
+            $(".accomplishments:last").append(HTMLworkAccomplishmentsEntry.replace(dataTmpl, work.jobs[job].accomplishments[accomplishment]));
           }
+        }
       }
+    }
   }
-}
+};
 
 
 var projects = {
   "projects": [
-      {
-        "title": "XML to Avro",
-        "dates": "July - September 2014",
-        "description": "Tool to generate an Avro schema from an XML Schema, and store any XML documents following that schema in Avro."
-      },
-      {
-        "title": "Virtual Reality eCommerce",
-        "dates": "January - May 2005",
-        "description": "Designed and developed the server side of a prototype QuickTime VR three-dimensional interface to an online store. Researched different virtual reality environments and settled on QuickTime VR. Coded a web services client for the Amazon Web Services service on the server in Java / JSP."
-      }
+    {
+      "title": "XML to Avro",
+      "dates": "July - September 2014",
+      "description": "Tool to generate an Avro schema from an XML Schema, and store any XML documents following that schema in Avro."
+    },
+    {
+      "title": "Virtual Reality eCommerce",
+      "dates": "January - May 2005",
+      "description": "Designed and developed the server side of a prototype QuickTime VR three-dimensional interface to an online store. Researched different virtual reality environments and settled on QuickTime VR. Coded a web services client for the Amazon Web Services service on the server in Java / JSP."
+    }
   ],
   "display": function() {
-      if (projects.projects && projects.projects.length > 0) {
-          for (project in projects.projects) {
-              $("#projects").append(HTMLprojectStart);
-              $(".project-entry:last").append(HTMLprojectTitle.replace(dataTmpl, projects.projects[project].title));
-              $(".project-entry:last").append(HTMLprojectDates.replace(dataTmpl, projects.projects[project].dates));
-              $(".project-entry:last").append(HTMLprojectDescription.replace(dataTmpl, projects.projects[project].description));
-              if (projects.projects[project].image) {
-                  $(".project-entry:last").append(HTMLprojectImage.replace(dataTmpl, projects.projects[project].image));
-              }
-          }
+    if (projects.projects && projects.projects.length > 0) {
+      for (project in projects.projects) {
+        $("#projects").append(HTMLprojectStart);
+        $(".project-entry:last").append(HTMLprojectTitle.replace(dataTmpl, projects.projects[project].title));
+        $(".project-entry:last").append(HTMLprojectDates.replace(dataTmpl, projects.projects[project].dates));
+        $(".project-entry:last").append(HTMLprojectDescription.replace(dataTmpl, projects.projects[project].description));
+        if (projects.projects[project].image) {
+          $(".project-entry:last").append(HTMLprojectImage.replace(dataTmpl, projects.projects[project].image));
+        }
       }
+    }
   }
-}
+};
 
 function inName() {
-    var nameArray = bio.name.split(' ');
-    var outName =
-         nameArray[0].substring(0, 1).toUpperCase()  + nameArray[0].substring(1)
-           + " " + nameArray[1].toUpperCase();
-    return outName;
+  var nameArray = bio.name.split(' ');
+  var outName =
+    nameArray[0].substring(0, 1).toUpperCase()  + nameArray[0].substring(1)
+      + " " + nameArray[1].toUpperCase();
+  return outName;
 }
 
 bio.display();
